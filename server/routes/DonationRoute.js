558eca6bde588082b7febfbe802c7c0b7068f8c6
Tharushi <it21099854@my.sliit.lk>
2023-04-14 10:24:21 +0530

    commit-donationRoute-server

--- server/routes/DonationRoute.js
+++ server/routes/DonationRoute.js
@@ -57,7 +57,61 @@ router.post('/add', upload.fields([{ name: 'paymentSlip' }]), async (req, res) =
   }
 });
 
+// GET all donations
+router.get('/', async (req, res) => {
+  try {
+    // Retrieve all donations from the database
+    const donations = await Donation.find();
 
+    // Extract the required fields from each donation
+    const donationData = donations.map((donation) => ({
+      issuename: donation.issuename,
+      fullname: donation.fullname,
+      nic: donation.nic,
+      gender: donation.gender,
+      email: donation.email,
+      phoneNumber: donation.phoneNumber,
+      bank: donation.bank,
+      branch: donation.branch,
+      amount: donation.amount,
+      paymentSlip: donation.paymentSlip,
+      _id: donation._id, // Add the _id field
+    }));
+
+    res.status(200).json(donationData);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'An error occurred while retrieving the data' });
+  }
+});
+
+// GET a single donation by id
+router.get("/:id", getDonation, (req, res) => {
+  res.json(res.donation);
+});
+
+// CREATE a new donation
+router.post("/", async (req, res) => {
+  const donation = new Donation({
+    issuename: req.body.issuename,
+    fullname: req.body.fullname,
+    nic: req.body.nic,
+    gender: req.body.gender,
+    email: req.body.email,
+    phoneNumber: req.body.phoneNumber,
+    bank: req.body.bank,
+    branch: req.body.branch,
+    amount: req.body.amount,
+    paymentSlip: req.body.paymentSlip,
+  });
+
+  try {
+    const newDonation = await donation.save();
+    res.status(201).json(newDonation);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
 
 // UPDATE an existing donation by id
 router.put('/:id', getDonation, async (req, res) => {
@@ -113,7 +167,19 @@ router.put('/:id', getDonation, async (req, res) => {
   }
 });
 
-
+// DELETE an existing donation by id
+router.delete('/:id', async (req, res) => {
+  try {
+    const deletedDonation = await Donation.findByIdAndRemove(req.params.id);
+    if (!deletedDonation) {
+      return res.status(404).json({ error: 'Donation not found' });
+    }
+    res.status(200).json({ message: 'Donation deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
 
 // Middleware function to get a donation by id
 async function getDonation(req, res, next) {
